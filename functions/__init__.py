9d613bd5597191f375164b5dce66dc6aa6edeaae
mhbonnet <marie.h.bonnet@gmail.com>
2021-03-04 09:46:44 +0100

    adding structure and html

---
+++ functions/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import re
+import nltk
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+import spacy
+
+def find_tags(title=None, body=None):
+    """Process the title and body provided
+    Returns the proposed tags"""
+    clean_title = preprocess_words(title)
+    clean_body = preprocess_words(body)
+
+    return ['python', 'flask', 'nlp']
+
+
+def preprocess_words(text):
+    """Cleans the text provided and
+    returns a string containing all the meaningful words"""
+
+    # 1. enlever html
+    text1 = BeautifulSoup(text, 'lxml').get_text() 
+    
+    # 2. on ne garde que lettres (pas ponctuation/chiffres)     
+    text2 = re.sub("[^a-zA-Z]", " ", text1) 
+
+    # 3. minuscules et découpage en mots
+    words = text2.lower().split()                             
+    
+    # 4. stopwords : conversion set (rapidité) + enlever
+    sw = set(stopwords.words("english"))                  
+    interest_words = [w for w in words if not w in sw]
+    
+    # 5. Spacy lemmatization
+    nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+    words_lem_spacy = [nlp(w)[0].lemma_ for w in interest_words] 
+
+    # 6. Join the words back into one string separated by space, 
+    # and return the result.
+    return( " ".join(words_lem_spacy))  
--- tags_api.py
+++ tags_api.py
@@ -1,40 +1,32 @@
 # -*- coding: utf-8 -*-
 
-import json
-import requests
-from flask import Flask, request
+from flask import Flask, request, render_template
+from functions import find_tags
 
 app = Flask(__name__)
 
 # Avec Flask, les décorateurs sont utilisés pour 
-# associer une URL à une fonction. Ici, on associe 
-# donc la fonction  welcome  à l’URL /
+# associer une URL à une fonction. 
+# Exemple : fonction welcome associée à l’URL /
 @app.route("/")
 def welcome():
     return "Welcome to the test api!"
 
 @app.route('/tags_api', methods=['GET', 'POST'])
 def form_extract():
-    # POST request (après action submit)
+    # POST request
     if request.method == 'POST':
         titre = request.form.get('title_input')
-        question = request.form.get('body_input')
-        tags = ['html', 'css', 'flask', 'json', 'api']
-        return '''
-                  <h3>Titre = {}</h3>
-                  <p>{}</p>
-                  <h1>Tags proposés :</h1>
-                  <p>{}</p>'''.format(titre, question, tags)
+        question = request.form.get('body_input')      
+        tags = find_tags(titre, question)
+        
+        return render_template('tags_answer.html',
+                               title=titre,
+                               body=question,
+                               tags=tags)
 
-    # GET request (formulaire)
-    return '''
-              <form method="POST">
-                  <div>
-                  <label>Titre: <input type="text" name="title_input" required
-       minlength="2" maxlength="256" size="50"</label></div>
-                  <div><label>Question:  <textarea name="body_input" rows="10" cols="100"></textarea></label></div>
-                  <input type="submit" value="Soumettre">
-              </form>'''
-
+    # GET request ou erreur
+    return render_template('tags_form.html')
+   
 if __name__ == "__main__":
     app.run(debug=True)
